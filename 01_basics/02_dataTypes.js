65aba30c95e0ebbd7ccfa5f2abee2f0838882846
AryanTheng <162592894+AryanTheng@users.noreply.github.com>
2025-01-27 13:55:49 +0000

    dataTypes lec 5 completed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node js not brouser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Aryan")
+
+let name = "Aryan";
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53 range
+// bigint
+// string => ""
+// boolean  => true/false
+// null => standlone value
+// undefined => value not assigned
+// symbol => nuique
+
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
